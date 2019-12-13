74567a5f5c92c4c98467634a894218909612c739
Aysa Grabert <aysagrabert@Aysas-MacBook-Pro.local>
2019-12-13 15:10:07 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+
+import CurrencyConverter from './Components/CurrencyConverter'
+import CurrencyDisplay from './Components/CurrencyDisplay'
 
 class App extends Component {
-  render() {
-    return (
-      <div>
+	render() {
+		return (
+			<>
+				<h2>Render Props</h2>
+				<CurrencyConverter
+					render={(currencyData, amount) => (
 
-      </div>
-    );
-  }
+						<CurrencyDisplay currencyData={currencyData} amount={amount} />
+					)}
+				/>
+			</>
+		)
+	}
 }
 
-export default App;
+export default App
